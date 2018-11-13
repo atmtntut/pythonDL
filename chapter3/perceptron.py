436d667df1217db8b251abed3ec40ccb129915d9
atmtntut <shui514@qq.com>
2018-11-13 23:18:32 +0800

    尝试突变函数

---
+++ chapter3/perceptron.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def step_func(x):
+    y = x > 0
+    return y.astype(np.int)
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+x = np.arange(-5.0, 5.0, 0.1)
+ys = []
+ys.append(step_func(x))
+ys.append(sigmoid(x))
+ys.append(relu(x))
+for y in ys:
+    plt.plot(x, y)
+#plt.ylim(-0.1, 1.1)
+plt.show()
